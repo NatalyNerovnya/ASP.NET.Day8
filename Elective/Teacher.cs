cbc2d4f761c1b78a71bdf79f94b5217965448efd
Наташа Неровня <natanerovnja@gmail.com>
2016-04-14 20:32:02 +0300

    add nlog

--- Elective/Teacher.cs
+++ Elective/Teacher.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Elective
 {
     public class Teacher : IMentor
     {
+        #region NLog field
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
         #region Fields
         private ICourse course;
         private static readonly int maxNumberOfStudents = 30;
@@ -21,6 +26,7 @@ namespace Elective
             {
                 if(course == null)
                     throw new ArgumentNullException();
+                logger.Trace($"Return course {course.Name}");
                 return course;
             }
         }
@@ -35,25 +41,37 @@ namespace Elective
                 throw new ArgumentNullException();
             if(numberOfStudent < 0 || numberOfStudent > maxNumberOfStudents)
                 throw new AggregateException();
+            logger.Trace($"Open course {name} with group of {numberOfStudent} students");
             course = manager.CreateCourse(numberOfStudent,name, this);
         }
 
         public ICourse CloseCourse()
         {
             if (course.IsFinish == true)
+            {
+                logger.Info($"Closing {course.Name} course");
                 return null;
+            }
             else
             {
+                logger.Info($"Trying to close not finished {course.Name}");
                 return course;
             }
         }
 
         string IMentor.CheckHomework(bool done)
         {
+            logger.Trace($"Mentor check homework");
             if (done == true)
+            {
+                logger.Info("Course is passed");
                 return $"Course \"{Course.Name}\" is passed.";
+            }
             else
+            {
+                logger.Info("Course is passed");
                 return $"Course \"{Course.Name}\" isn't passed.";
+            }
         }
 #endregion
 
