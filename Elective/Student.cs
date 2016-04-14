8b97dfd4bfa3363a62cd7609eb2d169a456dba3e
Наташа Неровня <natanerovnja@gmail.com>
2016-04-14 10:25:16 +0300

    Add regions

--- Elective/Student.cs
+++ Elective/Student.cs
@@ -8,9 +8,12 @@ namespace Elective
 {
     public class Student : IStudent
     {
+        #region Fields
         private string name;
         private IArchive archive;
+        #endregion
 
+        #region Property
         public string Name
         {
             get { return name; }
@@ -21,31 +24,42 @@ namespace Elective
                 name = value;
             }
         }
+        #endregion
+
+        #region Constructors
 
         public Student(string name)
         {
+            if(name == null)
+                throw new ArgumentNullException();
             Name = name;
         }
 
         public Student(string name, ICourse course):this(name)
         {
+            CheckCourse(course);
             RegisterOnCourse(course);
         }
 
+        public Student(string name, IMentor mentor):this(name)
+        {
+            if(mentor == null)
+                throw new ArgumentNullException();
+            CheckCourse(mentor.Course);
+            RegisterOnCourse(mentor.Course);
+        }
+        #endregion
+
+        #region IStudent Implementation
         public bool ObserveCourse(ICourse course)
         {
-            return DoHomework();
-        }
-
-        public bool DoHomework()
-        {
-            //Какая-то логика на то, в каких случаях студент делает домашку
-            Console.WriteLine($"{Name}.DoHomework");
-            return true;
+            CheckCourse(course);
+            return ((IStudent)this).DoHomework();
         }
 
         public void RegisterOnCourse(ICourse course)
         {
+            CheckCourse(course);
             if (!IsArchiveExist())
             {
                 ArchiveManager manager = new FileArchiveManager();
@@ -54,9 +68,24 @@ namespace Elective
             course.ObserveStudents(this, archive);
         }
 
+        bool IStudent.DoHomework()
+        {
+            //Какая-то логика, в каких случаях студент делает домашку. В моем случаи все студенты молодцы
+            Console.WriteLine($"{Name}.DoHomework");
+            return true;
+        }
+        #endregion
+
+        #region Private Methods
+        private void CheckCourse(ICourse course)
+        {
+            if (course == null)
+                throw new ArgumentNullException();
+        }
         private bool IsArchiveExist()
         {
             return ReferenceEquals(archive, null) ? false : true;
         }
+#endregion
     }
 }
