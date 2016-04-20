bd27f1e9f5b83e79202be5836383fa2a44a94dda
Наташа Неровня <natanerovnja@gmail.com>
2016-04-20 22:27:35 +0300

    add fields and constructor

--- Elective/CourseManager.cs
+++ Elective/CourseManager.cs
@@ -8,6 +8,20 @@ namespace Elective
 {
     public abstract class CourseManager
     {
-        public abstract ICourse CreateCourse(int number,string name, IMentor mentor);
+        public string Name { get; private set; }
+        public int NumberOfStudents { get; private set; }
+        public IMentor Mentor { get; private set; }
+
+        public CourseManager(string name, int number, IMentor mentor)
+        {
+            if (number <= 0)
+                throw new ArgumentException();
+            if (name == null || mentor == null)
+                throw new ArgumentNullException();
+            Name = name;
+            NumberOfStudents = number;
+            Mentor = mentor;
+        }
+        public abstract ICourse CreateCourse();
     }
 }
